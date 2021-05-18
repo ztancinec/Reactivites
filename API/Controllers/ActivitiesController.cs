80f1adc16063a4a4638d534bc824f4d27e594800
Zvonimir Tančinec <zvonimir.tancinec@adnet.hr>
2021-05-18 16:07:46 +0200

    git branch -M main

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) 
+        {
+            return await context.Activities.FindAsync(id);
+        }
+    }
+}
